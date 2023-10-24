3980bab63fb945a0ce42536a120fd9f2013d5273
Muhammad Ridzan Pahmi <146890153+Ridzan12@users.noreply.github.com>
2023-10-24 13:57:02 +0700

    Add files via upload

---
+++ Import and Export.sql
@@ -0,0 +1,17 @@
+-- Create table learning and import the exist csv file.
+
+CREATE TABLE 
+	learning
+	(
+	a INTEGER,
+	b INTEGER,
+	c INTEGER
+	)
+	;
+
+--  after Import
+
+SELECT
+	*
+FROM
+	learning
